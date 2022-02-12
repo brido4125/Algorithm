2fdf82fe3098e0729d357c7969fde1d41b9f2488
hcs4125 <hcs4125@gmail.com>
2022-02-12 14:59:39 +0900

    update Fibonacci

---
+++ java_coding_test/src/string/Fibonacci.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Fibonacci {
+    private static int[] solution(int n) {
+        int[] answer = new int[n];
+        setFibonacciFormat(answer);
+        for (int i = 2; i < answer.length; i++) {
+            answer[i] = answer[i - 1] + answer[i - 2];
+        }
+        return answer;
+    }
+
+    private static void setFibonacciFormat(int[] ary) {
+        for (int i = 0; i <= 1; i++) {
+            ary[i] = 1;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Arrays.toString(solution(n)) = " + Arrays.toString(solution(n)));
+    }
+}
