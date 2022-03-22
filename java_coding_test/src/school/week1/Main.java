de72cd1b9b636fcf62295262ca843e7831933bf8
hcs4125 <hcs4125@gmail.com>
2022-03-22 13:58:13 +0900

    update:SelectionSort

--- java_coding_test/src/school/week1/Main.java
+++ java_coding_test/src/school/week1/Main.java
@@ -1,43 +1,40 @@
 package school.week1;
 
 public class Main {
-    private static int process(int command, int param1, int param2) {
-        int[][] school = new int[3][3];
-        int[][] student = new int[10000][3];
+    void run_contest() {
+        int schoolNum = 3;
+        int studentNum = 1000;
+        Point2D[] schoolPoints = setSchoolPoints(schoolNum);
+        Point2D[] studentPoints = setStudentPoints(studentNum);
+        //학생 좌표를 하나 불러 올때, 바로 학교 배정하기
 
-        int counter;
-
-        switch (command) {
-            case 1:
-                return (school[param1][0]);
-            case 2:
-                return (school[param1][1]);
-            case 3:
-                return (student[param1][0]);
-            case 4:
-                return (student[param1][1]);
-            case 5:
-                student[param1][2] = param2;
-                return 0;
+    }
+    private Point2D[] setSchoolPoints(int schoolNum) {
+        Point2D[] schools = new Point2D[schoolNum];
+        for (int i = 0; i < schoolNum; i++) {
+            schools[i] = get_random_position();
         }
+        return schools;
+    }
 
-        if (command == 0) {
-            for (counter = 0; counter < 3; counter++) {
-                school[counter][0] = (int)(Math.random() * 1000);
-                school[counter][1] = (int)(Math.random() * 1000);
-            }
-            for (counter = 0; counter < 10000; counter++) {
-                student[counter][0] = (int)(Math.random() * 1000);
-                student[counter][1] = (int)(Math.random() * 1000);
-                student[counter][2] = -1;
-            }
-
-            return 0;
+    private Point2D[] setStudentPoints(int studentNum) {
+        Point2D[] students = new Point2D[studentNum];
+        for (int i = 0; i < studentNum; i++) {
+            students[i] = get_random_position();
         }
-        return -1;
+        return students;
+    }
+
+    /**
+     * 좌표은 0~999까지
+     * @return
+     */
+    private Point2D get_random_position() {
+        return new Point2D((int)(Math.random()*1000),(int)(Math.random()*1000));
     }
 
     public static void main(String[] args) {
-
+        Main main = new Main();
+        main.run_contest();
     }
 }
---
+++ java_coding_test/src/school/week1/Point2D.java
@@ -0,0 +1,11 @@
+package school.week1;
+
+public class Point2D {
+    private int x;
+    private int y;
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
