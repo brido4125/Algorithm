41f421ee3a84133d0b84d29f0e13bb59eeba2280
hcs4125 <hcs4125@gmail.com>
2022-05-17 22:14:12 +0900

    update:graph

--- java_coding_test/src/basicSearch/LeafNode.java
+++ java_coding_test/src/basicSearch/LeafNode.java
@@ -1,5 +1,8 @@
 package basicSearch;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LeafNode {
     static Node root;
 
@@ -11,11 +14,34 @@ public class LeafNode {
         }
     }
 
+    public static int BFS(Node node) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+        int L = 0;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                Node cur = queue.poll();
+                if (cur.lt == null && cur.rt == null) {
+                    return L;
+                } else {
+                    queue.offer(cur.rt);
+                    queue.offer(cur.lt);
+                }
+            }
+            L++;
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         root = new Node(1);
         root.lt = new Node(2);
-        root.rt = new Node(2);
+        root.rt = new Node(3);
+        root.rt.lt = new Node(23);
+        root.rt.rt = new Node(21);
         root.lt.lt = new Node(4);
         root.lt.rt = new Node(5);
+        System.out.println(BFS(root));
     }
 }
