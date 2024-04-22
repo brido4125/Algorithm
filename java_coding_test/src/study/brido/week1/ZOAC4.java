da25a176f5b7e5091f589a74821c341828c703c8
brido4125 <hcs4125@gmail.com>
2024-04-22 22:18:07 +0900

    update:ZOAC4

--- java_coding_test/src/study/brido/week1/ZOAC4.java
+++ java_coding_test/src/study/brido/week1/ZOAC4.java
@@ -1,4 +1,26 @@
 package study.brido.week1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class ZOAC4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        //H, N
+        //W, M
+        int sero = (H - 1) / (N + 1);
+        int garo = (W - 1) / (M + 1);
+        int answer = (sero + 1) * (garo + 1);
+        System.out.println(answer);
+
+    }
 }
