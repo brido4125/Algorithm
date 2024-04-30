0d9bcc38864e85ac086b75a998492489368c969a
brido4125 <hcs4125@gmail.com>
2024-04-30 21:40:28 +0900

    update:3주차

--- java_coding_test/src/study/brido/week2/단어공부.java
+++ java_coding_test/src/study/brido/week2/단어공부.java
@@ -3,7 +3,6 @@ package study.brido.week2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -14,19 +13,22 @@ public class 단어공부 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         String s = st.nextToken();
-        String lowerCase = s.toLowerCase();
-        char[] charArray = lowerCase.toCharArray();
-        HashMap<Character, Integer> map = new HashMap<>();
+        String upperCase = s.toUpperCase();
+        char[] charArray = upperCase.toCharArray();
+        HashMap<Character, Long> map = new HashMap<>();
+
+        long max = 0;
+
         for (Character c : charArray) {
-            if (map.get(c) == null) {
-                map.put(c, 0);
+            map.put(c, map.getOrDefault(c, 0L) + 1);
+            if(max < map.get(c)) {
+                max = map.get(c);
             }
-            map.put(c, map.get(c) + 1);
         }
-        Integer max = map.values().stream().max(Comparator.naturalOrder()).get();
+
         int cnt = 0;
         Character c = null;
-        for (Map.Entry<Character, Integer> e : map.entrySet()) {
+        for (Map.Entry<Character, Long> e : map.entrySet()) {
             if (e.getValue() == max) {
                 cnt += 1;
                 c = e.getKey();
