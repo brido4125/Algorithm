a67e0dcad1fb2898f0e3645fede46e8c2cc6f35a
brido4125 <hcs4125@gmail.com>
2024-07-23 22:47:44 +0900

    update:fail

---
+++ java_coding_test/src/study/brido/solvedac/boj_2xN타일링2.java
@@ -0,0 +1,16 @@
+package study.brido.solvedac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_2xN타일링2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+    }
+}
