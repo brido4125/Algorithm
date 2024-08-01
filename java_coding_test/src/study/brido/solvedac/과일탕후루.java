d81433d208029504962bbfdd1c49cbbf423902ac
brido4125 <hcs4125@gmail.com>
2024-08-01 23:16:12 +0900

    update:과일탕후루 fail

---
+++ java_coding_test/src/study/brido/solvedac/과일탕후루.java
@@ -0,0 +1,20 @@
+package study.brido.solvedac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class 과일탕후루 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> deq = new ArrayDeque<>();
+
+    }
+}
