795519a46ab0750e846c560ac84f4fcd5b59f23e
brido4125 <hcs4125@gmail.com>
2024-08-03 14:42:08 +0900

    update:과일탕후루 fail

--- java_coding_test/src/study/brido/solvedac/과일탕후루.java
+++ java_coding_test/src/study/brido/solvedac/과일탕후루.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class 과일탕후루 {
@@ -14,7 +18,69 @@ public class 과일탕후루 {
 
         int N = Integer.parseInt(st.nextToken());
 
-        Deque<Integer> deq = new ArrayDeque<>();
+        Deque<Integer> src = new ArrayDeque<>();
 
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            src.add(Integer.parseInt(st.nextToken()));
+        }
+
+        List<Integer> answers = new ArrayList<>();
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>(src);
+        HashSet<Integer> set = new HashSet<>(deque);
+        while (set.size() > 2) {
+            Integer i = deque.pollFirst();
+            if (!deque.contains(i)) {
+                set.remove(i);
+            }
+            if (set.size() <= 2) {
+                break;
+            }
+            Integer l = deque.pollLast();
+            if (!deque.contains(l)) {
+                set.remove(l);
+            }
+        }
+        answers.add(deque.size());
+
+        deque = new ArrayDeque<>(src);
+        set = new HashSet<>(deque);
+        while (set.size() > 2) {
+            Integer i = deque.pollFirst();
+            if (!deque.contains(i)) {
+                set.remove(i);
+            }
+        }
+        answers.add(deque.size());
+
+        deque = new ArrayDeque<>(src);
+        set = new HashSet<>(deque);
+        while (set.size() > 2) {
+            Integer i = deque.pollLast();
+            if (!deque.contains(i)) {
+                set.remove(i);
+            }
+        }
+        answers.add(deque.size());
+
+        deque = new ArrayDeque<>(src);
+        set = new HashSet<>(deque);
+        while (set.size() > 2) {
+            Integer i = deque.pollLast();
+            if (!deque.contains(i)) {
+                set.remove(i);
+            }
+            if (set.size() <= 2) {
+                break;
+            }
+            Integer l = deque.pollFirst();
+            if (!deque.contains(l)) {
+                set.remove(l);
+            }
+        }
+        answers.add(deque.size());
+
+        System.out.println(Collections.max(answers));
     }
 }
