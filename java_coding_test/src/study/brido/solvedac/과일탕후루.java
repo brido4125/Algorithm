7cb75a6cca94321494f7d19f912de753b26508b8
brido4125 <hcs4125@gmail.com>
2024-08-10 14:37:26 +0900

    update:과일탕후루 fail

--- java_coding_test/src/study/brido/solvedac/과일탕후루.java
+++ java_coding_test/src/study/brido/solvedac/과일탕후루.java
@@ -3,54 +3,60 @@ package study.brido.solvedac;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class 과일탕후루 {
-
-    static Integer answer = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int[] per = new int[2];
-
-        Deque<Integer> src = new ArrayDeque<>();
+        int[] ary = new int[N];
         st = new StringTokenizer(br.readLine());
+
+
         for (int i = 0; i < N; i++) {
-            src.add(Integer.parseInt(st.nextToken()));
+            int t = Integer.parseInt(st.nextToken());
+            ary[i] = t;
         }
-        permutation(0, per, N, src);
-        System.out.println(answer);
-    }
 
-    // 중복 순열
-    static void permutation(int L, int[] per, int N, Deque<Integer> deque) {
-        if (L == 2) {
-            ArrayDeque<Integer> copy = new ArrayDeque<>(deque);
-            int firstPoll = per[0];
-            int lastPoll = per[1];
-
-            for (int i = 0; i < firstPoll; i++) {
-                copy.pollFirst();
+        for (int i = N; i >= 2; i--) {
+            Map<Integer, Integer> map = new HashMap<>();
+            int[] init = Arrays.copyOfRange(ary, 0, i);
+            for (Integer num : init) {
+                Integer v = map.putIfAbsent(num, 1);
+                if (v != null) {
+                    map.put(num, v + 1);
+                }
             }
 
-            for (int i = 0; i < lastPoll; i++) {
-                copy.pollLast();
+            if (map.size() <= 2) {
+                System.out.println(i);
+                return;
             }
 
-            if (new HashSet<>(copy).size() <= 2 && copy.size() >= answer ) {
-                answer = copy.size();
+
+            for (int start = 0; start < N - i; start++) {
+                int end  = start + i;
+                Integer startElem = map.get(ary[start]);
+                map.put(ary[start], startElem - 1);
+                if (map.get(ary[start]) == 0) {
+                    map.remove(ary[start]);
+                }
+
+                Integer endElem = map.putIfAbsent(ary[end], 1);
+                if (endElem != null) {
+                    map.put(ary[end], endElem + 1);
+                }
+
+                if (map.size() <= 2) {
+                    System.out.println(i);
+                    return;
+                }
             }
-            return;
-        }
-        for (int i = 0 ; i < N ; i++) {
-            per[L] = i;
-            permutation(L + 1, per, N, deque);
         }
     }
 }
