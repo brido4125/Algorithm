145a8abf86af8ebf563ade70f6e04c8a983c893a
brido4125 <hcs4125@gmail.com>
2024-08-11 23:57:57 +0900

    update:과일탕후루 fail

--- java_coding_test/src/study/brido/solvedac/과일탕후루.java
+++ java_coding_test/src/study/brido/solvedac/과일탕후루.java
@@ -3,7 +3,6 @@ package study.brido.solvedac;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -16,28 +15,23 @@ public class 과일탕후루 {
         int N = Integer.parseInt(st.nextToken());
         int[] ary = new int[N];
         st = new StringTokenizer(br.readLine());
-
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
             int t = Integer.parseInt(st.nextToken());
             ary[i] = t;
+            Integer v = map.putIfAbsent(t, 1);
+            if (v != null) {
+                map.put(t, v + 1);
+            }
         }
 
+        // 크기가 i인 sliding window
         for (int i = N; i >= 2; i--) {
-            Map<Integer, Integer> map = new HashMap<>();
-            int[] init = Arrays.copyOfRange(ary, 0, i);
-            for (Integer num : init) {
-                Integer v = map.putIfAbsent(num, 1);
-                if (v != null) {
-                    map.put(num, v + 1);
-                }
-            }
-
             if (map.size() <= 2) {
                 System.out.println(i);
                 return;
             }
-
 
             for (int start = 0; start < N - i; start++) {
                 int end  = start + i;
